a6771f1d31bdf737e5ddcdbcbaef020e517f89ea
syall <stevenyuan@noreply.com>
2018-04-21 13:31:51 -0400

    Hello

--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
 var changeStart = document.getElementById('start');
 
 chrome.storage.sync.get('color', function(data) {
-  changeStart.style.backgroundColor = "red";
+  changeStart.style.backgroundColor = "data.color";
   changeStart.setAttribute('value', data.color);
   changeStart.onclick = function(element) {
   let color = "red";
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'changeStart.style.backgroundColor = "' + color + '";'});
-  });
+        {code: 'document.getElementById("start").style.backgroundColor = "' + color + '";'});
+    });
   };
 });
 

0ece5473857ad305de8295f2bbd413f1630681ab
syall <stevenyuan@noreply.com>
2018-04-21 13:35:33 -0400

    hello

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ chrome.storage.sync.get('color', function(data) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'document.getElementById('start').style.backgroundColor = "' + color + '";'});
+        {code: 'document.getElementById('start').style.background-color = "' + color + '";'});
     });
   };
 });

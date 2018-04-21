44138385de15e30f7a3c6071d71c89e71060ab1d
syall <stevenyuan@noreply.com>
2018-04-21 15:05:19 -0400

    change initial color of start

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
+  chrome.storage.sync.set({color: '#ffffff'}, function() {
+    console.log('The color is white.');
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{

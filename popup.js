5ac24734f1bdfeb7b885f2a7a490e50bcd419943
syall <stevenyuan@noreply.com>
2018-04-21 15:32:18 -0400

    Quality of Life Changes

--- popup.js
+++ popup.js
@@ -1,19 +1,31 @@
 'use strict';
 
 let changeStart = document.getElementById('start');
+let changeEnd = document.getElementById('end');
 
-
-chrome.storage.sync.get('color', function(data) {
-  changeStart.style.backgroundColor = data.color;
-  changeStart.setAttribute('value', data.color);
-});
-
+//chrome.storage.sync.get('color', function(data) {
+changeStart.style.backgroundColor = "green";
+changeStart.setAttribute('value', "green");
+//});
+changeEnd.style.backgroundColor = "red";
+changeEnd.setAttribute('value', "red");
 
 changeStart.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     let changeStart = document.getElementById('start');
-    changeStart.style.backgroundColor = "red"; 
-    changeStart.setAttribute('value', "red");
+    changeStart.style.backgroundColor = "white"; 
+    changeStart.setAttribute('value', "white");
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: ''});
+  });
+};
+
+changeEnd.onclick = function(element) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    let changeEnd = document.getElementById('start');
+    changeEnd.style.backgroundColor = "white"; 
+    changeEnd.setAttribute('value', "white");
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: ''});

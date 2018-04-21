29202d9472bed14ba1aede4e13db309ece7b2db7
hx0rz <yisheng.zhuo@gmail.com>
2018-04-21 12:44:15 -0400

    test

--- popup.js
+++ popup.js
@@ -8,6 +8,6 @@
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+          //{code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
   };

d07084a04aaf67aca7954757f497fe323da1ce80
syall <stevenyuan@noreply.com>
2018-04-21 14:28:25 -0400

    Scratch

--- popup.js
+++ popup.js
@@ -1,10 +1,29 @@
+'use strict';
+
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
+
+/*
 function functionStart() {
   let changeStart = document.getElementById("start");
   changeStart.addEventListener("click", function(element) {
     changeStart.classList.add("start");
   });
 }
-
+*/
 /*
 var changeStart = document.getElementById('start');
 

fce18d1d55cff5399d91467ddd69320948c6c475
syall <stevenyuan@noreply.com>
2018-04-21 13:51:28 -0400

    hello

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
-var color = "red";
-document.getElementById("start").style.background-color = color;
-
+function functionStart() {
+  let changeStart = document.getElementById("start");
+  changeStart.onclick = function(element) {
+    changeStart.style.background-color.val("red");
+  };
+}
 
 /*
 var changeStart = document.getElementById('start');
@@ -9,11 +12,11 @@ chrome.storage.sync.get('color', function(data) {
   changeStart.style.backgroundColor = "data.color";
   changeStart.setAttribute('value', data.color);
   changeStart.onclick = function(element) {
-  let color = "red";
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.getElementById("start").style.background-color = "' + color + '";'});
+    let color = "red";
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.getElementById("start").style.background-color = "' + color + '";'});
     });
   };
 });
